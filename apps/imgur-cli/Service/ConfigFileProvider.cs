1340f3aea0966631175212d111b548b46c8e15de
ZeekoZhu <vaezt@outlook.com>
2023-07-08 18:15:05 +0800

    chore: remove deprecated code

--- apps/imgur-cli/Service/ConfigFileProvider.cs
+++ apps/imgur-cli/Service/ConfigFileProvider.cs
@@ -75,16 +75,6 @@ public record AppConfig
   public AppSettings Settings { get; set; } = new();
 }
 
-public class AppAuthToken : IOAuth2Token
-{
-  public required string AccessToken { get; init; }
-  public required int ExpiresIn { get; init; }
-  public required string TokenType { get; init; }
-  public required string RefreshToken { get; init; }
-  public required int AccountId { get; init; }
-  public required string AccountUsername { get; init; }
-}
-
 /// <summary>
 /// </summary>
 public class AppSettings
