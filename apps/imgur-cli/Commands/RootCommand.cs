fd627e13165dc2fdfc59977351eaac3c9be4a9b7
ZeekoZhu <vaezt@outlook.com>
2023-07-08 21:41:21 +0800

    chore: add --version print

--- apps/imgur-cli/Commands/RootCommand.cs
+++ apps/imgur-cli/Commands/RootCommand.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using McMaster.Extensions.CommandLineUtils;
+using Spectre.Console;
 using Zeeko.ImgurCli.Service;
 
 namespace Zeeko.ImgurCli.Commands;
@@ -10,13 +12,32 @@ namespace Zeeko.ImgurCli.Commands;
   typeof(DeleteCommand))]
 public class RootCommand : CommandBase
 {
+  [Option(
+    CommandOptionType.NoValue,
+    Template = "-v|--version",
+    Description = "Show version information")]
+  public bool Version { get; set; }
+
   public override Task<int> OnExecuteAsync(CommandLineApplication app)
   {
-    app.ShowHelp();
+    if (Version)
+    {
+      // get version from assembly
+      var version = GetType()
+        .Assembly
+        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+        ?.InformationalVersion ?? "unknown";
+      Cli.WriteLine(
+        $"Imgur CLI v{version}",
+        new Style(
+          foreground: Color.Green,
+          decoration: Decoration.Bold));
+    }
+
     return Task.FromResult(0);
   }
 
-  public RootCommand(LazyServiceProvider lazyServiceProvider) : base(
+  public RootCommand(ILazyServiceProvider lazyServiceProvider) : base(
     lazyServiceProvider)
   {
   }
