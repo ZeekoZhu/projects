26864cab2e0d3c0d2fb47e0b6ecd9abfccfa2634
ZeekoZhu <vaezt@outlook.com>
2023-08-10 09:03:20 +0800

    chore: add git cz

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+const apps = fs.readdirSync(path.resolve(__dirname, "apps"));
+module.exports = {
+  prompt: {
+    scopes: [...apps],
+  },
+};
