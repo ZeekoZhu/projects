f3423ceedc06ca1d40dfefba0a2e011281f3f99c
ZeekoZhu <vaezt@outlook.com>
2024-01-21 13:55:20 +0800

    chore(csv-transformer): fix lint errors

--- apps/csv-transformer/src/app/app.component.ts
+++ apps/csv-transformer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
--- apps/csv-transformer/src/app/controls/csv-transform-editor/csv-transform-editor.component.ts
+++ apps/csv-transformer/src/app/controls/csv-transform-editor/csv-transform-editor.component.ts
@@ -5,7 +5,6 @@ import {
   effect,
   inject,
   Input,
-  signal,
   Signal,
   ViewChild,
 } from '@angular/core';
