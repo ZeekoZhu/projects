51cd003a7fdd7c0dff4205b5aa069e702cb8e6cf
ZeekoZhu <vaezt@outlook.com>
2024-03-13 21:55:47 +0800

    fix(dev-ctx): correct command name

--- apps/dev-context/Program.fs
+++ apps/dev-context/Program.fs
@@ -4,14 +4,13 @@ open System.CommandLine.Parsing
 open Projects.DevContext.ContextProviders
 open Projects.DevContext.Core
 
-let rootCommand = RootCommand("dev-ctx")
+let rootCommand = RootCommand(Name = "dev-ctx")
 
 let getCommand = Command("get", "Get the current development context")
 rootCommand.AddCommand(getCommand)
 
-let commandProviders: IDevContextCommandProvider list = [
-    RepoContextCommandProvider()
-]
+let commandProviders: IDevContextCommandProvider list =
+  [ RepoContextCommandProvider() ]
 
 commandProviders
 |> List.iter (fun provider -> provider.AddGetCommands getCommand)
