3653a836afdf9835b3f2a8aed1df0f2e4eaa8e1a
ZeekoZhu <vaezt@outlook.com>
2024-06-13 16:08:30 +0800

    fix(avalonia-utils): improve x11 hidpi support

--- libs/avalonia-utils/X11Helper.cs
+++ libs/avalonia-utils/X11Helper.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using Path = System.IO.Path;
 
 namespace Projects.AvaloniaUtils;
 
@@ -6,32 +7,60 @@ public class X11Helper
 {
   private static ILogger Log => Serilog.Log.ForContext<X11Helper>();
 
-  public static int GetGdkWindowScalingFactor()
+  public static float GetWindowScalingFactor()
   {
+    var unscaledDpi = ReadXSettingsdValue("Gdk/UnscaledDPI");
+
+    var scalingFactor = ReadXSettingsdValue("Gdk/WindowScalingFactor");
+    switch (unscaledDpi, scalingFactor)
+    {
+      case (null, null):
+        Log.Warning(
+          "Unable to determine scaling factor from xsettingsd config. Defaulting to 1.0");
+        return 1;
+      case (null, { } factor):
+        Log.Information("Using Scaling factor: {Factor}", factor);
+        return factor;
+      case ({ } dpi, null):
+        Log.Information("Using DPI: {Dpi}", unscaledDpi);
+        return dpi / 1024 / 96;
+      case ({ } dpi, { } factor):
+        Log.Information(
+          "Using DPI x Scaling Factor: {Dpi} / 1024 / 96 x {Factor}",
+          unscaledDpi, factor);
+        return dpi / 1024 / 96 * factor;
+    }
+  }
+
+  private static float? ReadXSettingsdValue(string key)
+  {
+    var configFilePath = Path.Join(
+      Environment.GetEnvironmentVariable("HOME"),
+      ".config",
+      "xsettingsd",
+      "xsettingsd.conf");
+    var content = File.ReadLines(configFilePath);
+    var targetLine = content.FirstOrDefault(
+      line => { return line.StartsWith(key); });
+    // return int.Parse(targetLine?.Split(' ')[1].Trim() ?? defaultVal);
+    if (targetLine == null) return null;
+
     try
     {
-      var configFilePath = System.IO.Path.Join(
-        Environment.GetEnvironmentVariable("HOME"),
-        ".config",
-        "xsettingsd",
-        "xsettingsd.conf");
-      var content = File.ReadLines(configFilePath);
-      var targetLine = content.FirstOrDefault(
-        line => line.StartsWith("Gdk/WindowScalingFactor"));
-      return int.Parse(targetLine?.Split(' ')[1].Trim() ?? "1");
+      return float.Parse(targetLine.Split(' ')[1].Trim());
     }
     catch (Exception e)
     {
-      Log.Error(e, "Failed to get Gdk/WindowScalingFactor");
-      return 1;
+      Log.Error(e, "Failed to read {Key} from xsettingsd config",
+        key);
+      return null;
     }
   }
 
   public static void SetGlobalScalingFactor()
   {
     if (!PlatformHelper.IsLinux()) return;
-    var factor = GetGdkWindowScalingFactor();
-    Log.Information("Gdk/WindowScalingFactor: {Factor}", factor);
+    var factor = GetWindowScalingFactor();
     Environment.SetEnvironmentVariable(
       "AVALONIA_GLOBAL_SCALE_FACTOR",
       factor.ToString());
